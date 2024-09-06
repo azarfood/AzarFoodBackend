a95bf8e495c6573b62617f6b18469ab098844b0a
YusefBH <yusefb1382@gmail.com>
2024-09-06 13:08:11 +0330

    fit : add foods relation in restaurant model

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Restaurant extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
+    protected $fillable = [];
 
-    ];
+    //region Relations
+    public function foods(): HasMany
+    {
+        return $this->hasMany(Food::class, 'restaurant_id');
+    }
+
+    //endregion
 }
