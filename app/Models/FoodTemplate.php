0d23c6f17760771457d756f75edbc8d29a94694b
YusefBH <yusefb1382@gmail.com>
2024-09-06 13:10:24 +0330

    fit : add foods relation in FoodTamplate model

--- app/Models/FoodTemplate.php
+++ app/Models/FoodTemplate.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FoodTemplate extends Model
 {
     use HasFactory;
+
+
+    //region Relations
+    public function foods(): HasMany
+    {
+        return $this->hasMany(Food::class, 'template_id');
+    }
+
+    //endregion
 }
