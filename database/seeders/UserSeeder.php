29a5bdfc69a3213d01f47fbf7ca46bd67eefd8cb
YusefBH <yusefb1382@gmail.com>
2024-09-06 19:07:03 +0330

    Fix : add trancate table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Restaurant;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -9,6 +10,7 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        User::truncate();
         User::factory(20)->create();
     }
 }
