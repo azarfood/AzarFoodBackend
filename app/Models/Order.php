8b3f45a6229ab19f541014ad421f491aadde7a00
YusefBH <yusefb1382@gmail.com>
2024-09-06 15:12:07 +0330

    fit : add order_products relation in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,19 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         "meal",
         "rating"
     ];
 
     //region Relations
-    public function user():BelongsTo
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class , "user_id");
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function order_products(): HasMany
+    {
+        return $this->hasMany(OrderProducts::class, "order_id");
     }
     //endregion
 }
