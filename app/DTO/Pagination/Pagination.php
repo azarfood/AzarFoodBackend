611ee56854204947221b16eeabecfe34bf3faaa6
YusefBH <yusefb1382@gmail.com>
2024-09-12 20:14:17 +0330

    Feat : add Pagination Dto files

---
+++ app/DTO/Pagination/Pagination.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DTO\Pagination;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use Spatie\DataTransferObject\DataTransferObject;
+use Spatie\DataTransferObject\Exceptions\UnknownProperties;
+
+class Pagination extends DataTransferObject
+{
+    public PaginationLinks $links;
+    public array $data;
+    public PaginationMeta $meta;
+
+
+    public static function fromModelPaginatorAndData(
+        LengthAwarePaginator $paginator,
+        array                $data
+    ): Pagination {
+        try {
+            return new self([
+                'links' => PaginationLinks::fromPaginator($paginator),
+                'data' => $data,
+                'meta' => PaginationMeta::fromPaginator($paginator),
+            ]);
+        }catch (UnknownProperties $e){
+            throw new \RuntimeException($e->getMessage());
+        }
+
+    }
+}
---
+++ app/DTO/Pagination/PaginationLinks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DTO\Pagination;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use Spatie\DataTransferObject\DataTransferObject;
+
+class PaginationLinks extends DataTransferObject
+{
+    public string $first;
+    public string $last;
+    public ?string $prev;
+    public ?string $next;
+
+    /**
+     * @throws \Spatie\DataTransferObject\Exceptions\UnknownProperties
+     */
+    public static function fromPaginator(LengthAwarePaginator $paginator): PaginationLinks
+    {
+        $urls = $paginator->getUrlRange(1, $paginator->lastPage());
+        $array_key_first = array_key_first($urls);
+        $currentPage = $paginator->currentPage();
+
+        return new PaginationLinks([
+            'first' => $urls[$array_key_first],
+            'last' => end($urls),
+            'prev' => $urls[$currentPage - 1] ?? null,
+            'next' => $urls[$currentPage + 1] ?? null,
+        ]);
+    }
+}
