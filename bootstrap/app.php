f5d402e328efc84c7d998db0017f55647791f2d0
YusefBH <yusefb1382@gmail.com>
2024-10-05 17:32:20 +0330

    Fix : modify app.php (add 404 exception)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,6 +26,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->renderable(function (ValidationException $e) {
             return response()->json(
                 $e->validator->errors()
+                , 422);
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json(
+                $e->getMessage()
                 , 422);
         });
     })->create();
